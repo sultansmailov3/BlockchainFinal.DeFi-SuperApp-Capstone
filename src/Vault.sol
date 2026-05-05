cfb7fe535643da564283acb7ed5020a62dddae9f
smailov3 <sultansmailov3@gmail.com>
2026-05-05 19:16:20 +0000

    OpenZeppelin realisesion for invaraintss

---
+++ src/Vault.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {ERC4626, ERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract DeFiVault is ERC4626, Ownable {
+    constructor(IERC20 asset, address initialOwner)
+        ERC4626(asset)
+        ERC20("Vault LP Token", "vLPT")
+        Ownable(initialOwner)
+    {}
+
+    function depositYield(uint256 amount) external onlyOwner {
+        SafeERC20.safeTransferFrom(IERC20(asset()), msg.sender, address(this), amount);
+    }
+}
