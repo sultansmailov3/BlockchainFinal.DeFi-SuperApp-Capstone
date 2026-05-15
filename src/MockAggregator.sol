8dab59222bffe709ccfe4a29c5bc073a5dc7b595
onesixy <kairzanovbatyrhan17@gmail.com>
2026-05-16 01:49:56 +0300

    Create MockAggregator.sol

---
+++ src/MockAggregator.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+contract MockAggregator {
+    int256 private _price;
+    uint256 private _updatedAt;
+
+    constructor(int256 initialPrice) {
+        _price = initialPrice;
+        _updatedAt = block.timestamp;
+    }
+
+    function setPrice(int256 newPrice) external {
+        _price = newPrice;
+        _updatedAt = block.timestamp;
+    }
+
+    function setUpdatedAt(uint256 timestamp) external {
+        _updatedAt = timestamp;
+    }
+
+    function latestRoundData() external view returns (
+        uint80, int256, uint256, uint256, uint80
+    ) {
+        return (1, _price, block.timestamp, _updatedAt, 1);
+    }
+}
