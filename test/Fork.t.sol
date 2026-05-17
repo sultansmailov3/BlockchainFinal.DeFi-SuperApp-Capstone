f95065fc0e1e3f5f9272b0b27c7126cab5b72e33
ZhantoreGaineden <242028@astanait.edu.kz>
2026-05-17 21:15:01 +0000

    ci: add coverage and slither checks

--- test/Fork.t.sol
+++ test/Fork.t.sol
@@ -13,13 +13,7 @@ interface AggregatorV3Interface {
     function latestRoundData()
         external
         view
-        returns (
-            uint80 roundId,
-            int256 answer,
-            uint256 startedAt,
-            uint256 updatedAt,
-            uint80 answeredInRound
-        );
+        returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
 
     function decimals() external view returns (uint8);
 }
