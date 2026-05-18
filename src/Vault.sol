b583ea5e98da8536847f915515ca68652941336a
smailov3 <sultansmailov3@gmail.com>
2026-05-18 04:29:14 +0000

    feat: add ERC-721 ProtocolNFT with Yul assembly, Factory CREATE/CREATE2, verify script

--- src/Vault.sol
+++ src/Vault.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.20;
 
 import {ERC4626, ERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract DeFiVault is ERC4626, Ownable {
     constructor(IERC20 asset, address initialOwner)
